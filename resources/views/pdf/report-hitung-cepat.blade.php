f3695afa534981dd69e7019c5303802b12ee3e5b
triemaryanto <triemaryanto93@gmail.com>
2024-11-14 15:38:57 +0700

    update

--- resources/views/pdf/report-hitung-cepat.blade.php
+++ resources/views/pdf/report-hitung-cepat.blade.php
@@ -98,7 +98,7 @@
                     <td>{{ $row->kecamatanTPS->region_nm ?? null }}</td>
                     <td>{{ $row->desaTPS->region_nm ?? null }}</td>
                     <td class="text-right"> {{ number_format($row->total_b_1 ?? 0, 0, ',', '.') }}
-                        <br> ({{ number_format($row->perc_b_1 ?? 0, 2) }})
+                        <br> ({{ number_format($row->perc_b_1 ?? 0, 2) }} ' %')
                     </td>
                     <td class="text-right"> {{ number_format($row->total_b_2 ?? 0, 0, ',', '.') }}
                         {{-- <br> ({{ number_format($row->perc_b_2 ?? 0, 2) }}%) --}}
